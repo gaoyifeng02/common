4d9ae9aa964d109ff971066c7b8f8d3b260942ee
gaoyifeng02 <gaoyifeng02@126.com>
2025-06-25 16:23:09 +0800

    '1'

---
+++ src/main/java/com/gaoyifeng/commom/CommomApplication.java
@@ -0,0 +1,20 @@
+package com.gaoyifeng.commom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaoyifeng
+ * @Classname CommomApplication
+ * @Description TODO Commom服务启动类
+ * @Date 2025/6/14 16:55
+ * @Created by gaoyifeng
+ */
+@SpringBootApplication
+public class CommomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommomApplication.class, args);
+    }
+
+}
